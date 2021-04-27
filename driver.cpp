f2e1798061d3e5d0ee1667cde76a2e3c41ed889d
ephoang <82679333+ephoang@users.noreply.github.com>
2021-04-27 12:45:39 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -2,66 +2,21 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include "miniGit.cpp"
 using namespace std;
 
 void PrintMenu()
 {
-    cout << "0 - Displays menu options" << endl;
-    cout << "1 - Add a repository" << endl;
-    cout << "2 - Adds a file to the repository" << endl;
-    cout << "3 - Remove a repository" << endl;
-    cout << "4 - Removes a file from the repository" << endl;
-    cout << "5 - Commit your changes to the file" << endl;
-    cout << "6 - Checkout previous versions of the repository" << endl;
-    cout << "7 - Leave the program" << endl;
+    //initialize, add, remove, commit, checkout, quit
+    cout << "Menu options" << endl;
+    cout << "1 - Initialize" << endl;
+    cout << "2 - Add" << endl;
+    cout << "3 - Remove" << endl;
+    cout << "4 - Commit" << endl;
+    cout << "5 - Checkout" << endl;
+    cout << "6 - Leave the program" << endl;
     cout << endl;
     cout << "Enter your selection: " << endl;
-}
-
-void Checkout()
-{
-    int input;
-
-    cout << "Enter commit number: " << endl;
-    cin >> input;
-    //if(valid commit)
-        //overwrite files by files in directory
-    //doesn't have a function in .cpp and .hpp yet
-}
-
-void Commit()
-{
-    // traverse SLL in .cpp
-    //if doesn't exist in directory
-        //copy file to into directory, intialize
-    //does exist in directory
-        //check if it has been changed
-            //not changed; break
-            //changed, copy into directory, intialize and update SLL node
-}
-
-void RemoveFile()
-{
-    string fileName;
-
-    cout << "Enter name of file you want to delete: " << endl;
-    cin >> fileName;
-    
-    RemoveFile(*node, fileName);
-}
-
-void AddFile()
-{
-    string fileName;
-
-    cout << "Enter name of file you want to create:" << endl;
-    cin >> fileName;
-    
-    //some loop to check if file name is unique
-        // new SLL node with fil, repository, version number
-    // if not unique
-        //cout << "That name is already in the directory. Please chose a different name" << endl;
-        //cin >> fileName;
 }
 
 int main()
@@ -73,49 +28,26 @@ int main()
     cout << "Welcome to miniGit! We recommend you create a repository first. Please enter your selction below." << endl;
     PrintMenu();
     cin >> userInput;
-    
-    while(userInput != 7)
+    miniGit a;
+
+    while(userInput != 6)
     {
         switch(userInput)
         {
-            case 0:
-                PrintMenu();
-                cin >> userInput;
             case 1:
-                //add repository
-                PrintMenu();
-                cin >> userInput;
+                a.initialize();
             case 2:
-                void AddFile();
-                PrintMenu();
-                cin >> userInput;
+                a.addFile();
             case 3: 
-                //remove repository
-                PrintMenu();
-                cin >> userInput;
+                a.removeFile();
             case 4:
-                void RemoveFile();
-                PrintMenu();
-                cin >> userInput;
+                a.commitChange();
             case 5:
-                void Commit();
-                PrintMenu();
-                cin >> userInput;
+                a.checkout();
             case 6:
-                cout << "You will lose any local changes if you haven't made a commit. Do you want to continue?\n yes or no" << endl;
-                cin >> changeMind;
-                if(changeMind == "yes" || changeMind =="Yes")
-                {
-                    void Checkout();
-                }
-                PrintMenu();
-                cin >> userInput;
-            case 7:
                 break;
             default:
-                cout << "Invalid option. Please slect a valid option." << endl;
-                PrintMenu();
-                cin >> userInput;
+                cout << "Invalid option. Please select a valid option." << endl;
         }
     }
     //delete and clear everything
