d931e924decea9e9205c63ada3f777c27ec8b498
Grahkay <58275048+Grahkay@users.noreply.github.com>
2021-04-27 17:06:58 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -9,12 +9,11 @@ void PrintMenu()
 {
     //initialize, add, remove, commit, checkout, quit
     cout << "Menu options" << endl;
-    cout << "1 - Initialize repository" << endl;
-    cout << "2 - Add file" << endl;
-    cout << "3 - Remove file" << endl;
-    cout << "4 - Commit changes of a file" << endl;
-    cout << "5 - Checkout previous versions of a file" << endl;
-    cout << "6 - Leave the program" << endl;
+    cout << "1 - Add file" << endl;
+    cout << "2 - Remove file" << endl;
+    cout << "3 - Commit changes of a file" << endl;
+    cout << "4 - Checkout previous versions of a file" << endl;
+    cout << "5 - Leave the program" << endl;
     cout << endl;
     cout << "Enter your selection: " << endl;
 }
@@ -24,8 +23,25 @@ int main()
     int userInput;
     string filename;
     int choice;
+    char >> repo
     
-    cout << "Welcome to miniGit! We recommend you initialize a repository first. Please enter your selction below." << endl;
+    cout << " Welcome to miniGit! wouild you like to initalize a repository first?\n Y or N" << endl;
+    cin >> repo;
+    if(repo == 'Y')
+    {
+        a.initialize();
+    }
+    else if(repo == 'N')
+    {
+        cout << "Thank you for visting. Goodbye!" << endl;
+    }
+    else
+    {
+        cout << "Invalid option. Please select a valid option." << endl;
+        cin >> repo;
+    }
+    
+    cout << "Please enter your selction below." << endl;
     PrintMenu();
     cin >> userInput;
     miniGit a;
@@ -35,26 +51,22 @@ int main()
         switch(userInput)
         {
             case 1:
-                a.initialize();
-                PrintMenu();
-                cin >> userInput;
-            case 2:
                 cout << "Enter file name to add: " << endl;
                 cin >> filename;
                 a.addFile(filename);
                 PrintMenu();
                 cin >> userInput;
-            case 3: 
+            case 2:
                 cout << "Enter file name to remove: " << endl;
                 cin >> filename;
                 a.removeFile(filename);
                 PrintMenu();
                 cin >> userInput;
-            case 4:
+            case 3: 
                 a.commitChange();
                 PrintMenu();
                 cin >> userInput;
-            case 5:
+            case 4:
                 cout << "You will lose changes that you have not commited. Continue?\n 1 - Yes\n";
                 cin >> choice;
                 if(choice == 1)
@@ -66,8 +78,8 @@ int main()
                     break;
                 }
                 PrintMenu();
-                cin >> userInput;
-            case 6:
+                cin >> userInput
+            case 5:
                 break;
             default:
                 cout << "Invalid option. Please select a valid option." << endl;
